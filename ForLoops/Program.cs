12c92898f174f19bea0bf22015772e26028584f8
Brittanie Campos <bri2212040@maricopa.edu>
2022-06-22 12:37:57 -0700

    Add project files.

---
+++ ForLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿// for loop = repeats some code a FINITE amount of times
+
+/*
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+*/
+
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Happy New Year!");
+
+Console.ReadKey();
